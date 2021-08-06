4ea3e202cb465d7e4d8fb5dee12b20c26711ec9c
Josh Yandell <gingerbinger2@gmail.com>
2021-08-05 20:33:00 -0400

    added chessboard image and functionality for rendering to screen

--- chessproject/chessproject/chessproject.cpp
+++ chessproject/chessproject/chessproject.cpp
@@ -2,20 +2,39 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Chess");
+    RenderWindow window(VideoMode(1200, 800), "Chess");
 
-    sf::Event event;
+    Texture chessBoard;
+    if (!chessBoard.loadFromFile("chessboard.png"))
+    {
+        return EXIT_FAILURE;
+    }
+
+    Sprite boardSprite(chessBoard);
+
+    Event event;
 
     while (window.isOpen())
     {
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+
+            if (event.type == Event::Closed)
             {
                 window.close();
             }
+
+            
         }
+
+        window.clear();
+
+        window.draw(boardSprite);
+
+        window.display();
     }
 }
